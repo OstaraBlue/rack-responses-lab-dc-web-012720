488083076bdbdc9559e794af3724f31bffb207da
Sonata <ostarablue@gmail.com>
2020-02-15 18:55:25 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        resp.write Time.now
+        t = Time.now
+        if (t.hour).between?(0, 12)
+            resp.write "Good Morning!"
+        else (t.hour).between?(12, 24) 
+            resp.write "Good Afternoon!"
+        end
+        resp.finish
+    end
+end 
+
